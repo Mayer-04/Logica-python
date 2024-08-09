7c44463d831d53857df3d8cfe1f19674e2898ad2
Mayer-04 <andres.20.chaves@gmail.com>
2024-08-09 12:26:43 -0500

    nuevos comentarios en el código

--- fundamentos/tipos_datos/tipos.py
+++ fundamentos/tipos_datos/tipos.py
@@ -1,7 +1,24 @@
+"""
+* Anotaciones de tipos (type hints) son una forma de anotar el tipo de un valor. 
+Se utilizan para mejorar la legibilidad y la comprensión del código.
+
+-  Usar anotaciones de tipos mejora la legibilidad y facilita la detección de errores
+"""
+
 no_tipado = "No tipado"
+
 # Forzamos el tipo de dato de la variable - anotaciones de tipos
 tipado: str = "Tipado"
 
+# Cambiamos el tipo de dato de la variable - No genera error
 tipado = 1
-
 print(tipado)
+
+
+# Anotaciones de tipos en funciones
+def suma(a: int, b: int) -> int:
+    return a + b
+
+
+resultado = suma(1, 2)
+print("resultado: ", resultado)
