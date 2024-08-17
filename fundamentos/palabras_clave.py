7a8c33bab6fdb4634d7ee4f121d85db57b2ad6ce
Mayer-04 <andres.20.chaves@gmail.com>
2024-08-17 17:45:54 -0500

    organizando las palabras clave

---
+++ fundamentos/palabras_clave.py
@@ -0,0 +1,27 @@
+"""
+* Palabras clave en Python
+
+La palabras clave son términos o palabras reservados que tienen un significado especial en Python.
+- No pueden ser usadas para nombrar de variables, funciones o clases,
+ya que Python las reserva internamente para su funcionamiento.
+
+- La palabra clave `is` se utiliza para verificar si dos objetos son iguales (Si son el mismo objeto en memoria).
+- La palabra clave `def` se utiliza para definir una función.
+- La palabra clave `return` se utiliza para retornar un valor de una función.
+- La palabra clave `yield from` se utiliza para generar un generador de elementos.
+- La palabra clave `raise` se utiliza para lanzar una excepción.
+- La palabra clave `assert` se utiliza para verificar que una condición sea verdadera.
+- La palabra clave `pass` se utiliza para pasar un valor sin realizar ninguna operación.
+- La palabra clave `None` se utiliza para representar el valor `null`.
+- La palabra clave `in` verifica si un elemento está presente (o no) en una secuencia (lista, tupla, conjunto, etc.).
+_ La palabra clave `global` se utiliza para definir una variable global.
+_ La palabra clave `lambda` se utiliza para crear una función lambda.
+- La palabra clave `as` se utiliza para renombrar una variable o crear un alias.
+- La palabra clave `nonlocal` se utiliza para definir una variable local no local.Útil cuando tenemos funciones anidadas.
+- La palabra clave `del` se utiliza para eliminar una variable o un objeto.
+- La palabra clave `await` se utiliza para esperar a que una función se complete.
+- La palabra clave `async` se utiliza para definir una función asincróna.
+- La palabra clave `with` se utiliza para el manejo de recursos y asegura que se liberen correctamente.
+- La palabra clave `from` se utiliza para importar un módulo.
+- La palabra clave `import` se utiliza para importar un paquete.
+"""
