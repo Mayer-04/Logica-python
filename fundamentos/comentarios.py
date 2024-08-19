5a17a0ff5dc096549110dacf0645f1f421990d1f
Mayer-04 <andres.20.chaves@gmail.com>
2024-08-19 11:23:08 -0500

    agregando nuevo comentario

--- fundamentos/comentarios.py
+++ fundamentos/comentarios.py
@@ -9,3 +9,6 @@ Comentario de varias líneas o multilínea
 Cuando definimos una función o una clase y queremos definir que hace esa función o clase,
 usamos el Document String (docstring) dentro de la función o la clase.
 """
+
+# Si su código es lo suficientemente legible, no necesita comentarios.
+# Agregar comentarios innecesarios sólo hará que su código sea menos legible.
