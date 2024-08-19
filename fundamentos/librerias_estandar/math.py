c05b8102cb34e67562afbca60874a1b2e984ad87
Mayer-04 <andres.20.chaves@gmail.com>
2024-08-19 17:23:28 -0500

    agregando algunas funciones matemáticas

---
+++ fundamentos/librerias_estandar/math.py
@@ -0,0 +1,61 @@
+import math
+
+"""
+* Librería estandar - math
+
+Funciones incorporadas de la librería estandar `math`
+
+- math.sqrt: Retorna la raiz cuadrada de un número.
+- math.pow: Retorna el valor de `x` elevado a la potencia `y`.
+- math.pi: Retorna el valor de `pi`.
+- math.e: Retorna el valor de `e`.
+- math.log: Retorna el logaritmo natural de un número.
+- math.log10: Retorna el logaritmo base 10 de un número.
+- math.sin: Retorna el seno de un número.
+- math.cos: Retorna el coseno de un número.
+- math.tan: Retorna la tangente de un número.
+- math.degrees: Convierte un angulo en grados a radianes.
+- math.radians: Convierte un angulo en radianes a grados.
+- math.ceil: Redondea un número hacia arriba.
+- math.floor: Redondea un número hacia abajo.
+- math.trunc: Elimina los decimales de un número.
+- math.factorial: Retorna el factorial de un número.
+
+Utilice `help(math)` para obtener detalles sobre la librería estandar `math`.
+"""
+
+# Función incorporada `sqrt`
+# Retorna la raiz cuadrada de un número.
+print(math.sqrt(4))  # Salida: 2.0
+
+# Función incorporada `pow`
+# Retorna el valor de `x` elevado a la potencia `y`.
+print(math.pow(2, 3))  # Salida: 8.0
+
+# Función incorporada `pi`
+# Retorna el valor de `pi`.
+print(math.pi)  # Salida: 3.141592653589793
+
+# Función incorporada `e`
+# Retorna el valor de `e`.
+print(math.e)  # Salida: 2.718281828459045
+
+# Función incorporada `log`
+# Retorna el logaritmo natural de un número.
+print(math.log(10))  # Salida: 2.302585092994046
+
+# Función incorporada `log10`
+# Retorna el logaritmo base 10 de un número.
+print(math.log10(100))  # Salida: 2.0
+
+# Función incorporada `sin`
+# Retorna el seno de un número.
+print(math.sin(math.radians(90)))  # Salida: 1.0
+
+# Función incorporada `cos`
+# Retorna el coseno de un número.
+print(math.cos(math.radians(180)))  # Salida: -1.0
+
+# Función incorporada `tan`
+# Retorna la tangente de un número.
+print(math.tan(math.radians(45)))  # Salida: 1.0
